ab1e9e4dfd86734fd2e3835031da5adf8923834f
Dawood Arif <99706520+dawudarif@users.noreply.github.com>
2023-08-27 16:52:58 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Gardient from "./components/Gardient";
 import Chefs from "./components/LandingPage/Chefs";
 import Featured from "./components/LandingPage/Featured";
 import Testimonials from "./components/LandingPage/Testimonials";
-import WhyUs from "./components/LandingPage/WhyUS";
+import WhyUs from "./components/LandingPage/WhyUs";
 import MovingText from "./components/MovingText";
 
 export default function Home() {
